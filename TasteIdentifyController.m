e1a8c5dafef06f4d89b65b29ec8fac4530cb0644
morganhowell95 <morgan.howell95@gmail.com>
2015-01-05 18:51:45 -0600

    Functionality further extended prior to segue activation to final venue

--- TasteIdentifyController.m
+++ TasteIdentifyController.m
@@ -10,6 +10,7 @@
 #import "EatsOptions.h"
 #import "EatsChoice.h"
 #import "AFHTTPRequestOperationManager.h"
+#import "PreferenceSearchViewController.h"
 #define locuName   @"mjhowell@live.unc.edu"
 #define locuKey @"348b2f6cc9b7c265d94ed414fc4c689e812a73b7"
 
@@ -79,17 +80,17 @@
 //chooses the appropriate time setting based on current time
 - (NSString *) chooseTimePeriod: (NSMutableString *) finalTime hours: (int) hrs
 {
-    if(hrs>4 && hrs<12){
+    if(hrs<12){
         [finalTime appendFormat: @" %@", time[0]];
     }
     else if (hrs==12){
         [finalTime appendFormat: @" %@", time[1]];
     }
-    else if (hrs>12 && hrs< 4)
+    else if (hrs>12 && hrs< 16)
     {
         [finalTime appendFormat: @" %@", time[2]];
     }
-    else if(hrs>4 && hrs <9)
+    else if(hrs>=16 && hrs <17)
     {
         [finalTime appendFormat: @" %@", time[3]];
     }
@@ -220,6 +221,8 @@ didUpdateLocations:(NSArray *)locations {
         coordinates.text = newloc;
         self.latitude = location.coordinate.latitude;
         self.longitude = location.coordinate.longitude;
+        NSLog(@"Set Latitude: %f", self.latitude);
+        NSLog(@"Set Longitude: %f", self.longitude);
     }
 }
 
@@ -282,30 +285,110 @@ numberOfRowsInComponent:(NSInteger) component
     }
 }
 
-//Segue Controller Functionality - dependent on user choice (mood of appetite)
 
+//Prior to segue activation, JSON is retrieved via Locu API depending on certain filters and attributes (set in specificTasteIdentityRequest)
+//JSON objects are then dynamically loaded into the models and sent to the destination segue
+//The destination segue holds a subclassed UITableViewController embedded within a UIViewController which is loaded up using a mutable array from the JSON model objects constructed
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     NSLog(@"locu latitude: %f", self.latitude);
-     NSLog(@"locu latitude: %f", self.longitude);
+    NSLog(@"locu latitude: %f", self.longitude);
+    if (!(self.latitude) && !(self.longitude)){[self startStandardUpdates];}
     
+
+    [self specificTasteIdentityRequest:sender];
+
+    
+    //delegates objects to the destination segue controller
+    TasteIdentifyController *identity_controller = self;
+    PreferenceSearchViewController *dest = segue.destinationViewController;
+    if([segue.identifier isEqualToString:@"preferenceSearch"]){
+        
+        
+        
+        
+    }
+    
+}
+
+     
+/*- (IBAction)button01Action:(id)sender {
+    [self performSegueWithIdentifier:@"finalselect" sender:sender];
+}*/
+
+
+//Represents the preliminary "secret sauce" to sending out queries based on the user's current mood in a particular time setting
+//Currently the functionality is basic - only simple queries for certain items on the menu. However in the next update, these queries will start involving more complicated requests (more tailored to a person's appetite).
+- (void)specificTasteIdentityRequest: (id) sender
+{
+    if([[sender currentTitle] isEqualToString:@"Date Night"]){
+        self.JSONfeed = @{@"api_key" : locuKey, @"fields" : @[@"name", @"location", @"contact",@"extended"],@"venue_queries": @[@{@"location":@{@"geo":@{@"$in_lat_lng_radius":@[@(self.latitude),@(self.longitude),@(self.searchRadius)]}}}], @"menu_item_queries": @[@{@"name":@"steak"}]};
+        [self locuRequestOrigin];
+    }
+    
+    if([[sender currentTitle] isEqualToString:@"Girls Night"]){
+        self.JSONfeed = @{@"api_key" : locuKey, @"fields" : @[@"name", @"location", @"contact",@"extended"],@"venue_queries": @[@{@"location":@{@"geo":@{@"$in_lat_lng_radius":@[@(self.latitude),@(self.longitude),@(self.searchRadius)]}}}], @"menu_item_queries": @[@{@"name":@"wine"}]};
+        [self locuRequestOrigin];
+    }
+    
+    if([[sender currentTitle] isEqualToString:@"Baked"]){
+        self.JSONfeed = @{@"api_key" : locuKey, @"fields" : @[@"name", @"location", @"contact",@"extended"],@"venue_queries": @[@{@"location":@{@"geo":@{@"$in_lat_lng_radius":@[@(self.latitude),@(self.longitude),@(self.searchRadius)]}}}], @"menu_item_queries": @[@{@"name":@"cookies",@"name":@"pizza",@"name":@"burrito"}]};
+        [self locuRequestOrigin];
+    }
+    
+    if([[sender currentTitle] isEqualToString:@"Games On"]){
+        self.JSONfeed = @{@"api_key" : locuKey, @"fields" : @[@"name", @"location", @"contact",@"extended"],@"venue_queries": @[@{@"location":@{@"geo":@{@"$in_lat_lng_radius":@[@(self.latitude),@(self.longitude),@(self.searchRadius)]}}}], @"menu_item_queries": @[@{@"name":@"wings"}]};
+        [self locuRequestOrigin];
+    }
+    
+    if([[sender currentTitle] isEqualToString:@"Out Drinking"]){
+        self.JSONfeed = @{@"api_key" : locuKey, @"fields" : @[@"name", @"location", @"contact",@"extended"],@"venue_queries": @[@{@"location":@{@"geo":@{@"$in_lat_lng_radius":@[@(self.latitude),@(self.longitude),@(self.searchRadius)]}}}], @"menu_item_queries": @[@{@"name":@"alcohol",@"name":@"beer",@"name":@"shots",@"name":@"wine"}]};
+        [self locuRequestOrigin];
+    }
+    
+    if([[sender currentTitle] isEqualToString:@"Sober Up"]){
+        self.JSONfeed = @{@"api_key" : locuKey, @"fields" : @[@"name", @"location", @"contact",@"extended"],@"venue_queries": @[@{@"location":@{@"geo":@{@"$in_lat_lng_radius":@[@(self.latitude),@(self.longitude),@(self.searchRadius)]}}}], @"menu_item_queries": @[@{@"name":@"pizza",@"name":@"burrito",@"name":@"sandwich"}]};
+        [self locuRequestOrigin];
+    }
+    
+    if([[sender currentTitle] isEqualToString:@"On The Go"]){
+        self.JSONfeed = @{@"api_key" : locuKey, @"fields" : @[@"name", @"location", @"contact",@"extended"],@"venue_queries": @[@{@"location":@{@"geo":@{@"$in_lat_lng_radius":@[@(self.latitude),@(self.longitude),@(self.searchRadius)]}}}], @"menu_item_queries": @[@{@"name":@"hotdog",@"name":@"fries",@"name":@"cheeseburger"}]};
+        [self locuRequestOrigin];
+    }
     
     if([[sender currentTitle] isEqualToString:@"Studying"]){
         self.JSONfeed = @{@"api_key" : locuKey, @"fields" : @[@"name", @"location", @"contact",@"extended"],@"venue_queries": @[@{@"location":@{@"geo":@{@"$in_lat_lng_radius":@[@(self.latitude),@(self.longitude),@(self.searchRadius)]}}}], @"menu_item_queries": @[@{@"name":@"coffee"}]};
         [self locuRequestOrigin];
     }
     
+    if([[sender currentTitle] isEqualToString:@"Dessert"]){
+        self.JSONfeed = @{@"api_key" : locuKey, @"fields" : @[@"name", @"location", @"contact",@"extended"],@"venue_queries": @[@{@"location":@{@"geo":@{@"$in_lat_lng_radius":@[@(self.latitude),@(self.longitude),@(self.searchRadius)]}}}], @"menu_item_queries": @[@{@"name":@"dessert"}]};
+        [self locuRequestOrigin];
+    }
+    
+    if([[sender currentTitle] isEqualToString:@"Diet"]){
+        self.JSONfeed = @{@"api_key" : locuKey, @"fields" : @[@"name", @"location", @"contact",@"extended"],@"venue_queries": @[@{@"location":@{@"geo":@{@"$in_lat_lng_radius":@[@(self.latitude),@(self.longitude),@(self.searchRadius)]}}}], @"menu_item_queries": @[@{@"name":@"salad"}]};
+        [self locuRequestOrigin];
+    }
+    
+    if([[sender currentTitle] isEqualToString:@"Cheap"]){
+        self.JSONfeed = @{@"api_key" : locuKey, @"fields" : @[@"name", @"location", @"contact",@"extended"],@"venue_queries": @[@{@"location":@{@"geo":@{@"$in_lat_lng_radius":@[@(self.latitude),@(self.longitude),@(self.searchRadius)]}}}], @"menu_item_queries": @[@{@"name":@"burrito",@"name":@"cheeseburger",@"name":@"fast food"}]};
+        [self locuRequestOrigin];
+    }
+    
+    if([[sender currentTitle] isEqualToString:@"Sushi"]){
+        NSLog(@"Searching for great places to eat Sushi...");
+        self.JSONfeed = @{@"api_key" : locuKey, @"fields" : @[@"name", @"location", @"contact",@"extended"],@"venue_queries": @[@{@"location":@{@"geo":@{@"$in_lat_lng_radius":@[@(self.latitude),@(self.longitude),@(self.searchRadius)]}}}], @"menu_item_queries": @[@{@"name":@"sushi"}]};
+        [self locuRequestOrigin];
+    }
 }
-/*- (IBAction)button01Action:(id)sender {
-    [self performSegueWithIdentifier:@"finalselect" sender:sender];
-}*/
-
 
 //Execution method for all button actions 
 - (IBAction)ChoiceSelection:(id)sender {
+    [self performSegueWithIdentifier:@"preferenceSearch" sender:sender];
 }
 
-
+//exit to segue backwards
 - (IBAction)exit:(id)sender{
     [self dismissViewControllerAnimated:YES completion:nil];
 }
